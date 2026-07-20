4280176d8d07504ff7de8e6b5f3f33c8a9c4db33
Denver H. Myung <fe.dev.denver@gmail.com>
2026-07-21 07:40:11 +0900

    feat: add route based lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
+import { lazy, Suspense } from 'react'
 import { Route,Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
-import { HomePage } from './pages/HomePage'
-import { MovieDetailPage } from './pages/MovieDetailPage'
-import { NotFoundPage } from './pages/NotFoundPage'
-function App(){return <Layout><Routes><Route path="/" element={<HomePage/>}/><Route path="/movies/:movieId" element={<MovieDetailPage/>}/><Route path="*" element={<NotFoundPage/>}/></Routes></Layout>}
+const HomePage=lazy(()=>import('./pages/HomePage'))
+const MovieDetailPage=lazy(()=>import('./pages/MovieDetailPage'))
+const NotFoundPage=lazy(()=>import('./pages/NotFoundPage'))
+function App(){return <Layout><Suspense fallback={<p className="content">페이지를 불러오는 중입니다.</p>}><Routes><Route path="/" element={<HomePage/>}/><Route path="/movies/:movieId" element={<MovieDetailPage/>}/><Route path="*" element={<NotFoundPage/>}/></Routes></Suspense></Layout>}
 export default App
